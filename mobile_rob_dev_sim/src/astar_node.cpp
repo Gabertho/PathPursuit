cf499b04a086b60300a46668a73fe983b40c578e
Gabertho <gabrielbertho2014@hotmail.com>
2024-01-17 19:03:47 -0300

    updated project2

--- mobile_rob_dev_sim/src/astar_node.cpp
+++ mobile_rob_dev_sim/src/astar_node.cpp
@@ -1,71 +1,103 @@
 #include <chrono>
-
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <std_msgs/String.h>
 
 #include "astar/occupancy_map.h"
 #include "astar/astar.h"
 
 #define SUB_TOPIC_NAME_OCCUPANCY_MAP "/map"
 #define SUB_TOPIC_NAME_START_STATE "/initialpose"
-#define SUB_TOPIC_NAME_TARGET_STATE "/move_base_simple/goal"
 #define PUB_TOPIC_NAME_INFLATED_MAP "/inflated_map"
 #define PUB_TOPIC_NAME_OPTIMAL_PATH "/optimal_path"
+#define PUB_TOPIC_NAME_SETWAYPOINT "/set_waypoint"
 #define DEFAULT_MAP_THRESHOLD_VALUE 65
-#define DEFAULT_MAP_INFLATION_RADIUS 0.6 // meter
+#define DEFAULT_MAP_INFLATION_RADIUS 1.0 // meter
 
 class GridWorld
 {
 public:
     GridWorld();
     void ProcessQuery();
+    void SetTargetWaypoint(const std::string &waypoint_name);
 
 private:
     bool map_flag_ = false;
     bool source_point_flag_ = false;
-    bool target_point_flag_ = false;
     bool start_planning_flag_ = false;
     tf2::Vector3 source_point_ = {0, 0, 0};
     tf2::Vector3 target_point_ = {0, 0, 0};
+    std::string target_waypoint_name_;
     astar::Astar planner_;
     std::shared_ptr<occupancy_map::OccupancyMap> map_ptr_;
     ros::NodeHandle node_;
     ros::Subscriber sub_map_;
+    ros::Subscriber sub_waypoint_;
     ros::Subscriber sub_robot_pose_;
-    ros::Subscriber sub_target_point_;
     ros::Publisher pub_path_;
+    ros::Publisher pub_waypoint_;
     ros::Publisher pub_inflated_map_;
+    std::map<std::string, tf2::Vector3> waypoints_map_;
     void CallbackMap_(const nav_msgs::OccupancyGrid::ConstPtr &msg);
-    void CallbackTargetPoint_(const geometry_msgs::PoseStamped::ConstPtr &msg);
     void PublishPath_(const std::vector<tf2::Vector3> &path_w);
-    void CallbackRobotPose_(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg); // ou geometry_msgs::PoseWithCovarianceStamped p/ amcl
+    void CallbackRobotPose_(const nav_msgs::Odometry::ConstPtr &msg);
+    void CallbackWaypoint_(const std_msgs::String::ConstPtr &msg);
 };
 
 GridWorld::GridWorld()
 {
-    // Initilize subscribers and publishers
+    // Initialize subscribers and publishers
     sub_map_ = node_.subscribe(SUB_TOPIC_NAME_OCCUPANCY_MAP, 1, &GridWorld::CallbackMap_, this);
-    sub_robot_pose_ = node_.subscribe("/amcl_pose", 1, &GridWorld::CallbackRobotPose_, this); // ou o tópico apropriado
-    sub_target_point_ = node_.subscribe(SUB_TOPIC_NAME_TARGET_STATE, 1, &GridWorld::CallbackTargetPoint_, this);
+    sub_robot_pose_ = node_.subscribe("/odom", 1, &GridWorld::CallbackRobotPose_, this);
+    sub_waypoint_ = node_.subscribe("/set_waypoint", 1, &GridWorld::CallbackWaypoint_, this);
     pub_inflated_map_ = node_.advertise<nav_msgs::OccupancyGrid>(PUB_TOPIC_NAME_INFLATED_MAP, 1, this);
     pub_path_ = node_.advertise<nav_msgs::Path>(PUB_TOPIC_NAME_OPTIMAL_PATH, 1, this);
+    pub_waypoint_ = node_.advertise<std_msgs::String>(PUB_TOPIC_NAME_SETWAYPOINT, 1, this);
 
     // Get parameters
     ros::NodeHandle node_prv("~"); // private node for private params
     int occupancy_map_threshold;
-    double occupancy_map_inflatiion_radius;
+    double occupancy_map_inflation_radius;
     node_prv.param<int>("occupancy_map_threshold", occupancy_map_threshold, DEFAULT_MAP_THRESHOLD_VALUE);
-    node_prv.param<double>("occupancy_map_inflation_radius", occupancy_map_inflatiion_radius, DEFAULT_MAP_INFLATION_RADIUS);
+    node_prv.param<double>("occupancy_map_inflation_radius", occupancy_map_inflation_radius, DEFAULT_MAP_INFLATION_RADIUS);
 
     // Create an occupancy map object with parameters
-    map_ptr_ = std::make_shared<occupancy_map::OccupancyMap>(occupancy_map::OccupancyMap(occupancy_map_threshold, occupancy_map_inflatiion_radius));
+    map_ptr_ = std::make_shared<occupancy_map::OccupancyMap>(occupancy_map::OccupancyMap(occupancy_map_threshold, occupancy_map_inflation_radius));
 
-    // Initiate planner
+    // Initialize planner
     planner_ = astar::Astar(map_ptr_);
+
+    // Define waypoints
+    waypoints_map_ = {
+        {"LE-1", {-37.99, -5.45, 0.0}},
+        {"LE-2", {-30.15, -5.03, 0.0}},
+        {"LE-3", {-22.68, -4.45, 0.0}},
+        {"LE-4", {-15.36, -4.11, 0.0}},
+        // ... add more waypoints as needed
+    };
+}
+
+void GridWorld::SetTargetWaypoint(const std::string &waypoint_name)
+{
+    auto it = waypoints_map_.find(waypoint_name);
+    if (it != waypoints_map_.end())
+    {
+        target_waypoint_name_ = waypoint_name;
+        target_point_ = it->second;
+        ROS_INFO("Target waypoint set to: %s", waypoint_name.c_str());
+        start_planning_flag_ = true;
+    }
+    else
+    {
+        ROS_WARN("Waypoint %s not found.", waypoint_name.c_str());
+    }
 }
 
 void GridWorld::ProcessQuery()
@@ -108,16 +140,12 @@ void GridWorld::ProcessQuery()
     }
 }
 
-void GridWorld::CallbackRobotPose_(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg)
+void GridWorld::CallbackRobotPose_(const nav_msgs::Odometry::ConstPtr &msg)
 {
-    // Atualize source_point_ com a posição atual do robô
-    source_point_ = {msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z};
-    // Ajuste as flags conforme necessário
+    // Update source_point_ with the current position of the robot
+    source_point_ = {msg->pose.pose.position.x, msg->pose.pose.position.y, 0.0};
+    // Adjust flags as necessary
     source_point_flag_ = true;
-    if (map_flag_ && target_point_flag_)
-    {
-        start_planning_flag_ = true;
-    }
 }
 
 void GridWorld::CallbackMap_(const nav_msgs::OccupancyGrid::ConstPtr &msg)
@@ -138,21 +166,12 @@ void GridWorld::CallbackMap_(const nav_msgs::OccupancyGrid::ConstPtr &msg)
     // Set flags
     map_flag_ = true;
     source_point_flag_ = false;
-    target_point_flag_ = false;
-    start_planning_flag_ = false;
 }
 
-void GridWorld::CallbackTargetPoint_(const geometry_msgs::PoseStamped::ConstPtr &msg)
+void GridWorld::CallbackWaypoint_(const std_msgs::String::ConstPtr &msg)
 {
-    target_point_ = {msg->pose.position.x, msg->pose.position.y, msg->pose.position.z};
-    ROS_INFO("Received target point: x = %f, y = %f", target_point_.x(), target_point_.y());
-
-    target_point_flag_ = true;
-    if (map_flag_ && source_point_flag_)
-    {
-        start_planning_flag_ = true;
-        ROS_INFO("Target pose received, ready to start planning.");
-    }
+    SetTargetWaypoint(msg->data);
+    ProcessQuery();
 }
 
 void GridWorld::PublishPath_(const std::vector<tf2::Vector3> &path_w)
